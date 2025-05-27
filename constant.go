1b155fb25a777e5a42b2eab67dcbc996030f3390
simplyuix <simplyuix@gmail.com>
2025-05-27 22:07:31 +0530

    Constant

---
+++ constant.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() { 
+     
+	const PI = 3.15; 
+	const USER = "AMAN"
+	const days int = 7
+
+	const (
+		monday       = 1
+		tuesday      = 2
+		wenesday     = 3
+		thursday  = 4
+	)
+
+
+	fmt.Println(PI)
+}
