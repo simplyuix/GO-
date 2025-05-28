b08d2d6c68af9bce239baf6ff51c507be88903e9
simplyuix <simplyuix@gmail.com>
2025-05-28 14:44:26 +0530

    recurssion updated

--- recurssion.go
+++ recurssion.go
@@ -5,11 +5,20 @@ import (
 )
 
 func main() { 
+
+
 	fmt.Println("Let's Print Sum of First n numbers")
 	var N int64 
 	fmt.Scanln(&N)
 	val := sum(N)
 	fmt.Println(val)
+
+	fmt.Println("Let's Print fact of First n numbers")
+	fmt.Scanln(&N)
+	val = fact(N)
+	fmt.Println(val)
+
+
 
 
 }
@@ -18,4 +27,15 @@ func sum(N int64) (int64) {
 	} else {
 		return N+sum(N-1)
 	}
+}
+
+
+func fact(N int64) (int64) { 
+
+	if(N==0) {
+		return 1
+	} else {
+		return N*fact(N-1)
+	}
+
 }
